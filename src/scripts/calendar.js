809cc460e6bec09c12d1209ec46451608b721c7b
William Strandby Bauer <william.bauer02@gmail.com>
2025-03-19 14:47:36 +0100

    Does this work?

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let calendarEl = document.getElementById('calendar');
-    let calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
-    });
+  let calendarEl = document.getElementById('calendar');
+  let calendar = new FullCalendar.Calendar(calendarEl, {
+ initialView: 'resourceTimelineMonth',
+    resources: [
+      { id: 'a', title: 'Room A' },
+      { id: 'b', title: 'Room B' }  
+    ],
+  });
+  window.calendar = calendar;
     calendar.render();
 });
