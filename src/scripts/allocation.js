bd0f27d341a3934b6967188a03e11acf916394e2
William Strandby Bauer <wbauer24@student.aau.dk>
2025-03-27 08:56:08 +0100

    blyat

--- src/scripts/allocation.js
+++ src/scripts/allocation.js
@@ -1,5 +1,6 @@
 // addEvent (event objekt)
 /* event object: {title: title for event,    start: start-date,     end: end-date,    resourceIds: ID of the room}*/
+
 function allocate(testarray){
     console.log(testarray)
     for (let i = 0; i < testarray.length; i++){
