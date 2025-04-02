cc4ced33be1aecefeca6d3d482a768b9f3b7392f
David Bach <60426203+DrMachoo@users.noreply.github.com>
2025-04-02 09:01:38 +0200

    some prommises

--- src/getInfo.js
+++ src/getInfo.js
@@ -1,16 +1,26 @@
-import { readFile } from 'fs';
+import { readFile } from 'fs/promises';
 
+let Rooms = null;
+let Bookings = null;
 
+async function loadData() {
+    try {
+        const roomsData = await readFile('src/json/rooms.json', 'utf8');
+        const bookingsData = await readFile('src/json/bookings.json', 'utf8');
 
-readFile("src/json/rooms.json", 'utf8', (err, data) => { // Use 'utf8' encoding
-    if (err) throw err;
-    let roomData = JSON.parse(data); // Parse the JSON text into an object
-    console.log(roomData); // Log the parsed object
-});
+        Rooms = JSON.parse(roomsData);
+        Bookings = JSON.parse(bookingsData);
 
+    } catch (err) {
+        console.error("Error reading files:", err);
+    }
+}
 
-readFile("src/json/rooms.json", 'utf8', (err, data) => { // Use 'utf8' encoding
-    if (err) throw err;
-    let roomData = JSON.parse(data); // Parse the JSON text into an object
-    console.log(roomData); // Log the parsed object
-});
+// Load data at startup
+await loadData();
+
+console.log("Rooms Loaded:", Rooms);
+console.log("Bookings Loaded:", Bookings);
+// Now, Rooms and Bookings will have data after loading
+
+export { Rooms, Bookings };
