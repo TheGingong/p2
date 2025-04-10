2003c4b7125a21cdf723e4b0b78d44e1d909b2ff
Gakki <mathias@kejser.eu>
2025-04-10 10:36:50 +0200

    Ny funktion getBookingsAtDate

--- src/scripts/assignBookings.js
+++ src/scripts/assignBookings.js
@@ -1,16 +1,13 @@
 import fs from 'fs/promises';
 
-// Function to match bookings to rooms, reads and writes to JSON
-async function matchBookingsToRooms() {
+async function matchBookingsToRooms() {   
     // Defining paths to JSON files
     const roomsPath = "../json/rooms.json";
     const bookingsPath = "../json/bookings.json";
 
     try {
         // Reads from JSON files and checks for errors
-        console.log("Reading bookings from:", bookingsPath);
         const bookingsRaw = await fs.readFile(bookingsPath, 'utf-8');
-        console.log("Reading rooms from:", roomsPath);
         const roomsRaw = await fs.readFile(roomsPath, 'utf-8');
 
         if (!bookingsRaw.trim() || !roomsRaw.trim()) {
@@ -19,12 +16,8 @@ async function matchBookingsToRooms() {
         }
         
         // Parses the JSON content 
-        const bookings = JSON.parse(bookingsRaw);
+        const allBookings = JSON.parse(bookingsRaw);
         const rooms = JSON.parse(roomsRaw);
-
-        console.log(`Loaded ${bookings.length} bookings from ${bookingsPath}`);
-        console.log(`Loaded ${rooms.length} rooms from ${roomsPath}`);
-
         // Sort bookings by earliest enddate
         sortBookings(bookings);
 
@@ -33,16 +26,15 @@ async function matchBookingsToRooms() {
             booking.resourceIds = await assignResId(booking, rooms);
         }
 
-        console.log("Writing updated bookings to:", bookingsPath);
         // Updating resourceIds in bookings, to the newly assigned rooms
         await fs.writeFile(bookingsPath, JSON.stringify(bookings, null, 2));
         await fs.writeFile(roomsPath, JSON.stringify(rooms, null, 2));
-        console.log("Bookings updated successfully!");
 
     } catch (error) {
         console.error("Error updating bookings:", error);
     }
 }
+
 
 // Function that finds the best fit room, and returns its room number
 async function assignResId(booking, rooms) {
@@ -70,8 +62,11 @@ function sortBookings(bookings){
     })
 }
 
-//function sortBookings(bookings){
-//    bookings.sort((a, b) => new Date(a.endDate) - new Date(b.endDate));
-//}
+function getBookingsAtDate(bookings,date){
+    const visibleBookings = bookings.filter((booking) => bookings.dayOfBooking === date);
+    return visibleBookings
+}
 
 matchBookingsToRooms();
+
+
