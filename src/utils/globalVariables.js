1b03e7dbcbabb8bba3945b5d87088290e70e9643
Venil0u <vpeder24@student.aau.dk>
2025-05-09 15:15:11 +0200

    added soem prefs

--- src/utils/globalVariables.js
+++ src/utils/globalVariables.js
@@ -38,9 +38,16 @@ let globalState = {
 // Available room/booking preferences
 let roomTypes = ["s1q0","s2q0","s0q1","s1q1","s0q2"]; // Bed layouts/room types
 let totalPrefs = { // Object containing all the soft preference options
-    pref1 : ["opt1.1", "opt1.2", "opt1.3", "opt1.4", "opt1.5"],
-    pref2 : ["opt2.1", "opt2.2", "opt2.3", "opt2.4", "opt2.5"],
-    pref3 : ["opt3.1", "opt3.2", "opt3.3", "opt3.4", "opt3.5"]
+    pref1  : ["opt1.1", "opt1.2", "opt1.3", "opt1.4", "opt1.5"],
+    pref2  : ["opt2.1", "opt2.2", "opt2.3", "opt2.4", "opt2.5"],
+    pref3  : ["opt3.1", "opt3.2", "opt3.3", "opt3.4", "opt3.5"],
+    pref4  : ["opt4.1", "opt4.2", "opt4.3", "opt4.4", "opt4.5"],
+    pref5  : ["opt5.1", "opt5.2", "opt5.3", "opt5.4", "opt5.5"],
+    pref6  : ["opt6.1", "opt6.2", "opt6.3", "opt6.4", "opt6.5"],
+    pref7  : ["opt7.1", "opt7.2", "opt7.3", "opt7.4", "opt7.5"],
+    pref8  : ["opt8.1", "opt8.2", "opt8.3", "opt8.4", "opt8.5"],
+    pref9  : ["opt9.1", "opt9.2", "opt9.3", "opt9.4", "opt9.5"],
+    pref10 : ["opt10.1", "opt10.2", "opt10.3", "opt10.4", "opt10.5"]
 };
 
 // Global variable to change chances of preferences occuring. Probability will be 1/prefOdds
